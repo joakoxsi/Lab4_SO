42e499ffcb014f01e4e8c684b208158fc12e94da
Joakoxsi <29667290+joakoxsi@users.noreply.github.com>
2025-06-27 11:10:35 -0400

    Inicio de la Creacion de la Class Celula y creacion del archivo

--- LAB4_Munoz_Rojo.py
+++ LAB4_Munoz_Rojo.py
@@ -1,7 +1,21 @@
 import os ,random,threading
 
 
-nombre_output=["aislamiento","ronda_","diagnostico_final","acciones_anticuerpo"]
+class Celula:
+
+    def __init__(self,id,tipo):
+        self.id =id
+        self.tipo=tipo
+    
+    def __str__(self):
+        return f"Célula(ID={self.id}, Tipo={self.tipo})"
+    
+    def datos(self):
+        return (self.id,self.tipo)
+
+
+
+nombre_archivo=["aislamiento","ronda_","diagnostico_final","acciones_anticuerpo"]
 tipo={"Alien":0,"Humano":1,"Infectado":3}
 tipo_arreglo=["Alien","Humano"]
 
@@ -28,38 +42,50 @@ def combate(celula1,celula2):
 
 
 
-def archivos_creacion(nombre,Nronda=0):
+def archivos_creacion(nombre,lista_celulas,Nronda=0,):
 
-    if Nronda==0:
+    if Nronda==0 or Nronda==6:
         f=open(f"{nombre}.txt","w")
     else:
         f=open(f"{nombre}{Nronda}.txt","w")
 
+    for celula in lista_celulas:
+        celula_n=celula.datos()
+
+        f.write(f"La célula número {celula_n[0]} es del tipo {celula_n[1]} \n")
+
 
 numero_alien_inicial=16
-arreglo_celular=[]
+arreglo_celulas=[]
 
 contador_celulas_humanas=0
 numero_celulas_totales=0
-
-for _ in range(516):
-
+id_celula=0
+for _ in range(512):
+    id_celula+=1
 
     if (numero_alien_inicial > 0) and ((contador_celulas_humanas <= 25) and (contador_celulas_humanas >= 10) ):
         probablidad_tipo=random.randint(0,1)
         if probablidad_tipo == 0:
-            arreglo_celular.append(tipo_arreglo[probablidad_tipo])
+            arreglo_celulas.append(Celula(id_celula,tipo_arreglo[probablidad_tipo]))
             numero_alien_inicial-=1
             contador_celulas_humanas=0
         else:
-            arreglo_celular.append(tipo_arreglo[1])
+            arreglo_celulas.append(Celula(id_celula,tipo_arreglo[1]))
             contador_celulas_humanas+=1
     else:
-        arreglo_celular.append(tipo_arreglo[1])
+        arreglo_celulas.append(Celula(id_celula,tipo_arreglo[1]))
         contador_celulas_humanas+=1
+    
 
-print(arreglo_celular)
-        
+
+for celula_propia in arreglo_celulas:
+    print(celula_propia.datos())
+
+
+archivos_creacion(nombre_archivo[0],arreglo_celulas)
+
+
 
 
 
